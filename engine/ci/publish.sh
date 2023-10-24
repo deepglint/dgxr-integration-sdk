3a89d861b5393c0ecf76797a74d50df0b037f449
bulk <61551331+wodeshijie1@users.noreply.github.com>
2023-10-24 16:54:03 +0800

    fix: delete

--- engine/ci/publish.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-npm login --registry=https://registry.npmjs.org/ --username="$CI_NPM_PRIVATE_USER" --password="$CI_NPM_PRIVATE_PASSWORD"
-npm config set registry http://package.nemoface.com/repository/unity
-npm publish
--- model/game/game.go
+++
@@ -1,9 +0,0 @@
-package game
-
-// 定义一个自定义类型作为枚举类型
-type GameName string
-
-// 定义常量作为枚举值
-const (
-	Skiing GameName = "skiing"
-)
