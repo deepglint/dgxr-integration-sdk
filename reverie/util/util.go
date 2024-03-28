74b6db2c38f46b3a2a425fad530494dc51b928c6
bulk <zhichaoliu@deepglint>
2024-03-28 10:27:40 +0800

    fix: 增加空间 x

--- reverie/util/util.go
+++ reverie/util/util.go
@@ -20,3 +20,27 @@ func Get(url string) (data []byte, err error) {
 	}
 	return
 }
+
+func UnifyCoordinate(xDirection, yDirection string, pose []float32) []float64 {
+	switch {
+	case xDirection == "left" && yDirection == "up":
+		return []float64{-float64(pose[0]), float64(pose[1]), float64(pose[2])}
+	case xDirection == "left" && yDirection == "down":
+		return []float64{-float64(pose[0]), -float64(pose[1]), float64(pose[2])}
+	case xDirection == "right" && yDirection == "up":
+		return []float64{float64(pose[0]), float64(pose[1]), float64(pose[2])}
+	case xDirection == "right" && yDirection == "down":
+		return []float64{float64(pose[0]), -float64(pose[1]), float64(pose[2])}
+	case xDirection == "up" && yDirection == "left":
+		return []float64{float64(pose[1]), -float64(pose[0]), float64(pose[2])}
+	case xDirection == "up" && yDirection == "right":
+		return []float64{-float64(pose[1]), float64(pose[0]), float64(pose[2])}
+	case xDirection == "down" && yDirection == "left":
+		return []float64{-float64(pose[1]), -float64(pose[0]), float64(pose[2])}
+	case xDirection == "down" && yDirection == "right":
+		return []float64{float64(pose[1]), -float64(pose[0]), float64(pose[2])}
+	default:
+		// 默认情况，假设 x 和 y 方向都为右上方向
+		return []float64{float64(pose[0]), float64(pose[1]), float64(pose[2])}
+	}
+}
