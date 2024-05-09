89259425ba9d9507bab3108aa38042799e5c97dd
thomugo <menghaozhang@deepglint.com>
2024-05-09 15:25:03 +0800

    feat: xr test

---
+++ dgxr/Assets/Scripts/Deepglint/XR/Test.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Deepglint.XR
+{
+    public class Test : MonoBehaviour
+    {
+        public string name;
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
