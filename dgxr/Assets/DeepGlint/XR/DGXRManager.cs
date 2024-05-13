51d65906b55ca930420f60cda5d37c9090f38519
bulk <zhichaoliu@deepglint>
2024-05-13 11:54:45 +0800

    修改windows编辑器下走ws服务

--- dgxr/Assets/DeepGlint/XR/DGXRManager.cs
+++ dgxr/Assets/DeepGlint/XR/DGXRManager.cs
@@ -58,6 +58,21 @@ namespace DeepGlint.XR
             }
         }
 
+          
+        private void OnEnable()
+        {
+            Global.OnMetaPoseDataReceived += OnMetaPoseDataReceived;
+            Global.OnMetaPoseDataLost += OnMetaPoseDataLost;
+        }
+
+        // 在禁用对象时取消订阅事件
+        private void OnDisable()
+        {
+            Global.OnMetaPoseDataReceived -= OnMetaPoseDataReceived;
+            Global.OnMetaPoseDataLost -= OnMetaPoseDataLost;
+        }
+
+        
         public void OnDestroy()
         {
             if (!Application.isEditor && !Global.SystemName.Contains("Mac"))
