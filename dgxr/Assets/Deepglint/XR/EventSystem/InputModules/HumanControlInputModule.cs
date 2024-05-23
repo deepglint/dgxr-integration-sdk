e4ff35b7fe0d761e0b75bb4e5db48cba49f0a7cc
张梦豪 <thomugo@qq.com>
2024-05-23 15:23:25 +0800

    feat: HumanControlInputModule 支持IPointerEnterHandler IPointerExitHandler 事件的判定

--- dgxr/Assets/Deepglint/XR/EventSystem/InputModules/HumanControlInputModule.cs
+++ dgxr/Assets/Deepglint/XR/EventSystem/InputModules/HumanControlInputModule.cs
@@ -1,66 +1,355 @@
+using System.Collections.Generic;
 using Deepglint.XR.Inputs;
+using Deepglint.XR.Inputs.Devices;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using Vector3 = UnityEngine.Vector3;
 
 namespace Deepglint.XR.EventSystem.InputModules
 {
+    [AddComponentMenu("Event/Human Control Input Module")]
     public class HumanControlInputModule : PointerInputModule
     {
-        private PointerEventData _eventData;
-        
-        protected override void Start()
+        [SerializeField]
+        private float footTouchThreshold = 0.03f;
+
+        private const float DoubleClickTime = 0.3f;
+        protected override void OnEnable()
         {
-            base.Start();
-            _eventData = new PointerEventData(eventSystem);
-        } 
+            base.OnEnable();
+            DeviceManager.OnDeviceLost += OnDeviceLost;
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnEnable();
+            DeviceManager.OnDeviceLost -= OnDeviceLost;
+        }
         
         public override void Process()
         {
-            HandleHumanControlInput();
+            ProcessHumanControlEvent();
+        }
+
+        private void OnDeviceLost(int deviceId)
+        {
+            m_PointerData.Remove(-deviceId);
+            m_PointerData.Remove(deviceId);
         }
         
-        private void HandleHumanControlInput()
+        private void ProcessHumanControlEvent()
         {
             if (DeviceManager.m_ActiveDeviceCount > 0)
             {
                 foreach (var device in DeviceManager.AllActiveXRHumanDevices)
                 {
-                    // handle left foot
-                    _eventData.Reset();
-                    _eventData.pointerId = device.deviceId;
-                    var result = HandleFootInput(device.HumanBody.LeftFoot.position.value);
-                    if (result == null)
-                    {
-                        // handle right foot
-                        _eventData.Reset();
-                        result = HandleFootInput(device.HumanBody.RightFoot.position.value);
-                    }
+                    // PointerEventData left = GetFootPointerEventData(device);
+                    // ProcessMove(left);
+                    // PointerEventData right = GetFootPointerEventData(device, true);
+                    // ProcessMove(right);
+
+                    var humanData = GetHumanControlState(device);
+                    var leftFootData = humanData.GetButtonState(HumanPointerEventData.InputButton.LeftFoot).EventData;
+                    //ProcessFootTouch(leftFootData);
+                    ProcessHumanMove(leftFootData.ButtonData);
+                    //ProcessDrag(leftFootData.ButtonData);
                     
-                    HandlePointerExitAndEnter(_eventData, result);
+                    var rightFootData = humanData.GetButtonState(HumanPointerEventData.InputButton.RightFoot).EventData;
+                    //ProcessFootTouch(rightFootData);
+                    ProcessHumanMove(rightFootData.ButtonData);
+                    //ProcessDrag(rightFootData.ButtonData);
                 }
             }
         }
 
-        private GameObject HandleFootInput(Vector3 foot)
+        private void ProcessFootTouch(HumanButtonEventData data)
         {
-            _eventData.displayIndex = Global.Space.Bottom.UICamera.targetDisplay;
-            _eventData.position = WorldToBottomUIPosition(foot);
-            // 射线投射检测UI命中
-            eventSystem.RaycastAll(_eventData, m_RaycastResultCache);
-            var raycastResult = FindFirstRaycast(m_RaycastResultCache);
-            _eventData.pointerCurrentRaycast = raycastResult;
+            var pointerEvent = data.ButtonData;
+            var currentOverGo = pointerEvent.pointerCurrentRaycast.gameObject;
+            if (data.PressedThisFrame())
+            {
+                pointerEvent.eligibleForClick = true;
+                pointerEvent.delta = Vector2.zero;
+                pointerEvent.dragging = false;
+                pointerEvent.useDragThreshold = true;
+                pointerEvent.pressPosition = pointerEvent.position;
+                pointerEvent.pointerPressRaycast = pointerEvent.pointerCurrentRaycast;
+                
+                // todo DeselectIfSelectionChanged
+                
+                var resetDiffTime = Time.unscaledTime - pointerEvent.clickTime;
+                if (resetDiffTime >= DoubleClickTime)
+                {
+                    pointerEvent.clickCount = 0;
+                }
+                
+                var newPressed = ExecuteEvents.ExecuteHierarchy(currentOverGo, pointerEvent, ExecuteEvents.pointerDownHandler);
+                var newClick = ExecuteEvents.GetEventHandler<IPointerClickHandler>(currentOverGo);
+                if (newPressed == null)
+                    newPressed = newClick;
+                
+                float time = Time.unscaledTime;
+                
+                if (newPressed == pointerEvent.lastPress)
+                {
+                    var diffTime = time - pointerEvent.clickTime;
+                    if (diffTime < DoubleClickTime)
+                        ++pointerEvent.clickCount;
+                    else
+                        pointerEvent.clickCount = 1;
+
+                    pointerEvent.clickTime = time;
+                }
+                else
+                {
+                    pointerEvent.clickCount = 1;
+                }
+                
+                pointerEvent.pointerPress = newPressed;
+                pointerEvent.rawPointerPress = currentOverGo;
+                pointerEvent.pointerClick = newClick;
+                
+                pointerEvent.clickTime = time;
+                
+                pointerEvent.pointerDrag = ExecuteEvents.GetEventHandler<IDragHandler>(currentOverGo);
+
+                if (pointerEvent.pointerDrag != null)
+                    ExecuteEvents.Execute(pointerEvent.pointerDrag, pointerEvent, ExecuteEvents.initializePotentialDrag);
+            }
+
+            if (data.ReleasedThisFrame())
+            {
+                ExecuteEvents.Execute(pointerEvent.pointerPress, pointerEvent, ExecuteEvents.pointerUpHandler);
+
+                // see if we mouse up on the same element that we clicked on...
+                var pointerClickHandler = ExecuteEvents.GetEventHandler<IPointerClickHandler>(currentOverGo);
+
+                // PointerClick and Drop events
+                if (pointerEvent.pointerClick == pointerClickHandler && pointerEvent.eligibleForClick)
+                {
+                    ExecuteEvents.Execute(pointerEvent.pointerClick, pointerEvent, ExecuteEvents.pointerClickHandler);
+                }
+
+                if (pointerEvent.pointerDrag != null && pointerEvent.dragging)
+                {
+                    ExecuteEvents.ExecuteHierarchy(currentOverGo, pointerEvent, ExecuteEvents.dropHandler);
+                }
+
+                pointerEvent.eligibleForClick = false;
+                pointerEvent.pointerPress = null;
+                pointerEvent.rawPointerPress = null;
+                pointerEvent.pointerClick = null;
+
+                if (pointerEvent.pointerDrag != null && pointerEvent.dragging)
+                    ExecuteEvents.Execute(pointerEvent.pointerDrag, pointerEvent, ExecuteEvents.endDragHandler);
+
+                pointerEvent.dragging = false;
+                pointerEvent.pointerDrag = null;
+
+                // send exit events as we need to simulate this on touch up on touch device
+                ExecuteEvents.ExecuteHierarchy(pointerEvent.pointerEnter, pointerEvent, ExecuteEvents.pointerExitHandler);
+                pointerEvent.pointerEnter = null;
+            }
+        }
+
+        private void ProcessHumanMove(PointerEventData pointerData)
+        {
+            GameObject hoverTarget = pointerData.pointerCurrentRaycast.gameObject;
+            HandlePointerExitAndEnter(pointerData, hoverTarget);
+        }
+
+        private HumanControlState GetHumanControlState(DGXRHumanController device)
+        {
+            HumanControlState humanState = new HumanControlState();
+            PointerEventData leftFootData = GetFootPointerEventData(device);
+            PointerEventData rightFootData = GetFootPointerEventData(device, true);
+            humanState.SetButtonState(HumanPointerEventData.InputButton.LeftFoot, 
+                StateForHumanButton(device, HumanPointerEventData.InputButton.LeftFoot), 
+                leftFootData);
+            humanState.SetButtonState(HumanPointerEventData.InputButton.RightFoot, 
+                StateForHumanButton(device, HumanPointerEventData.InputButton.RightFoot), 
+                rightFootData);
+            return humanState;
+        }
+
+        private PointerEventData GetFootPointerEventData(DGXRHumanController device, bool right = false)
+        {
+            PointerEventData data;
+            Vector2 currentFootPos;
+            bool created;
+            if (right)
+            {
+                created = GetPointerData(device.deviceId, out data, true);
+                currentFootPos = WorldToBottomUIPosition(device.HumanBody.RightFoot.position.value);
+            }
+            else
+            {
+                created = GetPointerData(-device.deviceId, out data, true);
+                currentFootPos = WorldToBottomUIPosition(device.HumanBody.LeftFoot.position.value);
+            }
+            data.Reset();
+
+            if (created)
+            {
+                data.position = currentFootPos;
+                data.delta = Vector2.zero;
+            }
+            else
+            {
+                data.delta = currentFootPos - data.position;
+                data.position = currentFootPos;
+            }
+            
+            data.displayIndex = Global.Space.Bottom.UICamera.targetDisplay;
+            eventSystem.RaycastAll(data, m_RaycastResultCache);
+            data.pointerCurrentRaycast = FindFirstRaycast(m_RaycastResultCache);
             m_RaycastResultCache.Clear();
 
-            return raycastResult.gameObject;
-            // 处理进入和退出事件（可根据实际情况处理点击等其他事件）
-            //HandlePointerExitAndEnter(_eventData, raycastResult.gameObject); 
+            return data;
         }
 
         private Vector2 WorldToBottomUIPosition(Vector3 position)
         {
-            return new Vector2(position.x * 1920 / Global.Space.Bottom.Size.x + 960,
-                position.z * 1920 / Global.Space.Bottom.Size.y + 960);
+            // Debug.Log("foot height: " + position.y);
+            return new Vector2(
+                position.x * Global.Space.Bottom.Resolution.width / Global.Space.Bottom.Size.x 
+                + Global.Space.Bottom.Resolution.width * 0.5f, 
+                position.z * Global.Space.Bottom.Resolution.height / Global.Space.Bottom.Size.y 
+                + Global.Space.Bottom.Resolution.height * 0.5f);
+        }
+
+        private PointerEventData.FramePressState StateForHumanButton(DGXRHumanController device,
+            HumanPointerEventData.InputButton buttonId)
+        {
+            var touched = GetButtonDown(device, buttonId);
+            if (touched)
+            {
+                return PointerEventData.FramePressState.Pressed;
+            }
+            else
+            {
+                return PointerEventData.FramePressState.Released;
+            }
+            // todo set NotChanged
+        }
+
+        /// <summary>
+        /// Check if foot is touched on the floor
+        /// </summary>
+        /// <param name="device"></param>
+        /// <param name="buttonId"></param>
+        /// <returns></returns>
+        private bool GetButtonDown(DGXRHumanController device, HumanPointerEventData.InputButton buttonId)
+        {
+            // todo use action to check if perform and release stage happened in one frame. 
+            bool touched = false;
+            switch (buttonId)
+            {
+                case HumanPointerEventData.InputButton.LeftFoot:
+                    touched = device.HumanBody.LeftFoot.position.y.value < footTouchThreshold;
+                    break;
+                case HumanPointerEventData.InputButton.RightFoot:
+                    touched = device.HumanBody.RightFoot.position.y.value < footTouchThreshold;
+                    break;
+            }
+            
+            //Debug.Log(touched);
+
+            return touched;
+        }
+
+        private class HumanButtonState
+        {
+            public HumanButtonEventData EventData { get; set; }
+
+            public HumanPointerEventData.InputButton Button { get; set; } = HumanPointerEventData.InputButton.LeftFoot;
+        }
+        
+        /// <summary>
+        /// Information about a human button event.
+        /// </summary>
+        public class HumanButtonEventData
+        {
+            /// <summary>
+            /// The state of the button this frame.
+            /// </summary>
+            public PointerEventData.FramePressState ButtonState;
+
+            /// <summary>
+            /// Pointer data associated with the mouse event.
+            /// </summary>
+            public PointerEventData ButtonData;
+
+            /// <summary>
+            /// Was the button pressed this frame?
+            /// </summary>
+            public bool PressedThisFrame()
+            {
+                return ButtonState == PointerEventData.FramePressState.Pressed || ButtonState == PointerEventData.FramePressState.PressedAndReleased;
+            }
+
+            /// <summary>
+            /// Was the button released this frame?
+            /// </summary>
+            public bool ReleasedThisFrame()
+            {
+                return ButtonState == PointerEventData.FramePressState.Released || ButtonState == PointerEventData.FramePressState.PressedAndReleased;
+            }
+        }
+
+        private class HumanControlState
+        {
+            private List<HumanButtonState> _trackedButtons = new List<HumanButtonState>();
+            
+            public bool AnyPressesThisFrame()
+            {
+                var trackedButtonsCount = _trackedButtons.Count;
+                for (int i = 0; i < trackedButtonsCount; i++)
+                {
+                    if (_trackedButtons[i].EventData.PressedThisFrame())
+                        return true;
+                }
+                return false;
+            }
+            
+            public bool AnyReleasesThisFrame()
+            {
+                var trackedButtonsCount = _trackedButtons.Count;
+                for (int i = 0; i < trackedButtonsCount; i++)
+                {
+                    if (_trackedButtons[i].EventData.ReleasedThisFrame())
+                        return true;
+                }
+                return false;
+            }
+            
+            public HumanButtonState GetButtonState(HumanPointerEventData.InputButton button)
+            {
+                HumanButtonState tracked = null;
+                var trackedButtonsCount = _trackedButtons.Count;
+                for (int i = 0; i < trackedButtonsCount; i++)
+                {
+                    if (_trackedButtons[i].Button == button)
+                    {
+                        tracked = _trackedButtons[i];
+                        break;
+                    }
+                }
+
+                if (tracked == null)
+                {
+                    tracked = new HumanButtonState { Button = button, EventData = new HumanButtonEventData() };
+                    _trackedButtons.Add(tracked);
+                }
+                return tracked;
+            }
+            
+            public void SetButtonState(HumanPointerEventData.InputButton button, PointerEventData.FramePressState stateForHumanButton, PointerEventData data)
+            {
+                var toModify = GetButtonState(button);
+                toModify.EventData.ButtonState = stateForHumanButton;
+                toModify.EventData.ButtonData = data;
+            }
         }
     }
 }
---
+++ dgxr/Assets/Samples/HumanControlInputModule/Footprint.cs
@@ -0,0 +1,98 @@
+using System.Collections.Generic;
+using Deepglint.XR;
+using Deepglint.XR.Inputs;
+using UnityEngine;
+
+namespace Samples.HumanControlInputModule
+{
+    public class Footprint : MonoBehaviour
+    {
+        [SerializeField] 
+        private GameObject leftFootPrefab;
+        [SerializeField] 
+        private GameObject rightFootPrefab;
+
+        private RectTransform _rectTransform;
+        private Dictionary<int, RectTransform> _leftFoots;
+        private Dictionary<int, RectTransform> _rightFoots;
+        
+        private void OnDeviceLost(int deviceId)
+        {
+            if (_leftFoots.ContainsKey(deviceId))
+            {
+                Destroy(_leftFoots[deviceId].gameObject);
+                _leftFoots.Remove(deviceId);
+            }
+
+            if (_rightFoots.ContainsKey(deviceId))
+            {
+                Destroy(_rightFoots[deviceId].gameObject);
+                _rightFoots.Remove(deviceId);
+            }
+        }
+
+        private void OnEnable()
+        {
+            DeviceManager.OnDeviceLost += OnDeviceLost;
+        }
+
+        private void OnDisable()
+        {
+            DeviceManager.OnDeviceLost -= OnDeviceLost;
+        }
+
+        private Vector2 GetBottomUIPosition(Vector3 position)
+        {
+            return new Vector2(position.x * Global.Space.Bottom.Resolution.width / Global.Space.Bottom.Size.x, 
+                position.z * Global.Space.Bottom.Resolution.width / Global.Space.Bottom.Size.y);
+        }
+
+        public void Awake()
+        {
+            _rectTransform = gameObject.GetComponent<RectTransform>();
+            _leftFoots = new Dictionary<int, RectTransform>();
+            _rightFoots = new Dictionary<int, RectTransform>();
+        }
+
+        public void Update()
+        {
+            foreach (var device in DeviceManager.AllActiveXRHumanDevices)
+            {
+                RectTransform leftFootRectTransform;
+                if (!_leftFoots.ContainsKey(device.deviceId))
+                {
+                    // init foot;
+                    var leftFoot = Instantiate(leftFootPrefab, _rectTransform, false);
+                    leftFoot.SetActive(true);
+                    leftFootRectTransform = leftFoot.GetComponent<RectTransform>();
+                    _leftFoots[device.deviceId] = leftFootRectTransform;
+                }
+                else
+                {
+                    leftFootRectTransform = _leftFoots[device.deviceId];
+                }
+                leftFootRectTransform.anchoredPosition = GetBottomUIPosition(device.HumanBody.LeftFoot.position.value);
+                
+                RectTransform rightFootRectTransform;
+                if (!_rightFoots.ContainsKey(device.deviceId))
+                {
+                    // init foot;
+                    var rightFoot = Instantiate(rightFootPrefab, _rectTransform, false);
+                    rightFoot.SetActive(true);
+                    rightFootRectTransform = rightFoot.GetComponent<RectTransform>();
+                    _rightFoots[device.deviceId] = rightFootRectTransform;
+                }
+                else
+                {
+                    rightFootRectTransform = _rightFoots[device.deviceId];
+                }
+                rightFootRectTransform.anchoredPosition = GetBottomUIPosition(device.HumanBody.RightFoot.position.value);
+                
+                Vector3 eulerRotation = device.HumanPose.Rotation.value.eulerAngles;
+                Vector3 localEulerAngles = new Vector3(0, 0, -eulerRotation.y);
+                leftFootRectTransform.localEulerAngles = localEulerAngles;
+                rightFootRectTransform.localEulerAngles = localEulerAngles;
+            }
+        }
+    }
+}
