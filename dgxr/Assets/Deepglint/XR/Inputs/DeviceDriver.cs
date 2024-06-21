985e78ca99dcbce802d52654edbc52e9b9b62e5d
bulk <zhichaoliu@deepglint>
2024-06-21 10:50:22 +0800

    fix: 删除设备丢失焦点判断逻辑

--- dgxr/Assets/Deepglint/XR/Inputs/DeviceDriver.cs
+++ dgxr/Assets/Deepglint/XR/Inputs/DeviceDriver.cs
@@ -82,10 +82,6 @@ namespace Deepglint.XR.Inputs
             var device = DeviceManager.AddOrActiveDevice(data.BodyId, nameof(DGXRHumanController));
             if (device != null)
             {
-                if (!Application.isEditor && !Application.isFocused)
-                {
-                    return;
-                }
                 var xrDevice = device as DGXRHumanController;
                 if (xrDevice == null) return;
                 using (StateEvent.From(xrDevice, out var eventPtr))
