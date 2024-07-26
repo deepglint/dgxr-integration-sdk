7077c6843ba1e8ce8d8c195de290505f75634721
叶振邦 <zhenbangye@deepglint.com>
2024-07-26 15:40:00 +0800

    feat: add sentry

---
+++ Assets/UITest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.LogError("SDK-ERROR");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.LogError("SDK-ERROR-Update");
+    }
+}
