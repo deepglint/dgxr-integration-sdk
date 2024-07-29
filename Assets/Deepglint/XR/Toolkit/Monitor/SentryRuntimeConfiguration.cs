154510784de53014dd7fa3ba7d60c2d2284f4551
叶振邦 <zhenbangye@deepglint.com>
2024-07-29 14:31:30 +0800

    feat: add

--- Assets/Deepglint/XR/Toolkit/Monitor/SentryRuntimeConfiguration.cs
+++ Assets/Deepglint/XR/Toolkit/Monitor/SentryRuntimeConfiguration.cs
@@ -7,11 +7,19 @@ namespace Deepglint.XR.Toolkit.Monitor
     [CreateAssetMenu(fileName = "Assets/Resources/Sentry/SentryRuntimeConfiguration.asset", menuName = "Sentry/SentryRuntimeConfiguration", order = 999)]
     public class SentryRuntimeConfiguration : SentryRuntimeOptionsConfiguration
     {
+        
+        /// <summary>
+        /// 调用时机比Awake早，在Awake、Start赋值的变量无法获取
+        /// </summary>
+        /// <param name="options"></param>
         public override void Configure(SentryUnityOptions options)
         {
-            options.Environment = DGXR.Config.Space.Name;
+            var config = new Config.Config().InitConfig();
+            Debug.Log("Sentry Env: " + config.Space.Name);
+            options.Environment = config.Space.Name;
             options.CaptureInEditor = false;
             options.EnableLogDebouncing = true;
+            options.Debug = true;
         }
     }
 }
---
+++ Assets/UITest.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Deepglint.XR;
+using Deepglint.XR.Toolkit.Monitor;
+using UnityEngine;
+
+public class UITest : MonoBehaviour
+{
+    // private void Awake()
+    // {
+    //     SentryRuntimeConfiguration.EnvironmentName = DGXR.Config.Space.Name;
+    // }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.LogError("Sentry-build-Start"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.LogError("Sentry-build-Update");
+    }
+}
