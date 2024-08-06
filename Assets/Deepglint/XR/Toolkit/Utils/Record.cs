281394524adcc6c2832f4d20ad09357933081a3e
thomugo <menghaozhang@deepglint.com>
2024-08-06 16:34:57 +0800

    feat: update record filename

--- Assets/Deepglint/XR/Toolkit/Utils/Record.cs
+++ Assets/Deepglint/XR/Toolkit/Utils/Record.cs
@@ -1,5 +1,8 @@
 using System;
 using System.IO;
+using UnityEngine;
+using UnityEngine.WSA;
+using Application = UnityEngine.WSA.Application;
 
 namespace Deepglint.XR.Toolkit.Utils
 {
@@ -31,8 +34,8 @@ namespace Deepglint.XR.Toolkit.Utils
             string msg = (string)data;
             DateTime currentTime = DateTime.Now;
             currentTime = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, currentTime.Hour, currentTime.Minute, currentTime.Second);
-            string fileName = currentTime.ToString("yyyyMMddHHmmss");
-            string filePath = Path.Combine(_path, fileName + "_"+_marker+".txt");
+            string timestamp = currentTime.ToString("yyyyMMddHHmmss");
+            string filePath = Path.Combine(_path, $"{DGXR.AppName}_{timestamp}_{_marker}.txt");
             if (_currentHouse == -1)
             {
                 CheckFile(filePath);
@@ -43,8 +46,8 @@ namespace Deepglint.XR.Toolkit.Utils
             if (_currentHouse!=currentTime.Hour)
             {
                 currentTime = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, _currentHouse,currentTime.Minute, currentTime.Second);
-                string oldFileName = currentTime.ToString("yyyyMMddHHmmss");
-                string oldFilePath = Path.Combine(_path, oldFileName + "_"+_marker+".txt");
+                string oldTimestamp = currentTime.ToString("yyyyMMddHHmmss");
+                string oldFilePath = Path.Combine(_path,  $"{DGXR.AppName}_{oldTimestamp}_{_marker}.txt");
                 CheckFile(oldFilePath);
                 _writer.Close();
                 _writer = new StreamWriter(oldFilePath, true);
