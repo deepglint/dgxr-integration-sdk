d6dab0cd9eba24ffe1dd8373bceed5b3b6c2b48f
zhichaoliu <1481293524@qq.com>
2025-12-18 13:27:39 +0800

    修复退出冒泡到主线程

--- Assets/Deepglint/XR/Toolkit/DebugTool/VersionCode.cs
+++ Assets/Deepglint/XR/Toolkit/DebugTool/VersionCode.cs
@@ -6,7 +6,7 @@ namespace Deepglint.XR.Toolkit.DebugTool
     internal class VersionCode : MonoBehaviour
     {
         // CI发布时赋值
-        public const string SdkVersionCode = "";
+        public const string SdkVersionCode = "v2.10.4";
 
         private void OnGUI()
         {
